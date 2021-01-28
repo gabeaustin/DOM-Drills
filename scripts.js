e66879c5efc0a7bd81dcdfde214fb9e5a0c2d962
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-01-28 12:05:00 -0600

    added code to create and append h2 - h6

--- scripts.js
+++ scripts.js
@@ -1,18 +1,96 @@
-// Number 3
-let div = document.createElement("div");
-let headerContainer = div.className = "header-container";
+/* Number 4 */
+document.addEventListener("DOMContentLoaded", function() {
+    /* Number 3 */
+    let div = document.createElement("div");
+    let headerContainer = div.className = "header-container";
 
-// Number 4
-// Create a h1 element
-let h1 = document.createElement("h1");
-// Create a text node with the text "This is an h1"
-let h1Text = document.createTextNode("This is an h1.");
-// Append the text to the h1 
-h1.appendChild(h1Text);
-// append the h1 to the div
-div.appendChild(h1);
-// append the div to the body
-document.body.appendChild(div);
+    /* Number 4 */
+    // Create a h1 element
+    let h1 = document.createElement("h1");
+    // Create a text node with the text "This is an h1"
+    let h1Text = document.createTextNode("This is an h1.");
+    // Append the text to the h1 
+    h1.appendChild(h1Text);
+    // append the h1 to the div
+    div.appendChild(h1);
+    // append the div to the body
+    document.body.appendChild(div); // it will not show up w/o this line
+    // let h1ClassName = div.className = "h1";
+
+    /* Number 5 */
+    // create a h2 element
+    let h2 = document.createElement("h2");
+    // give it the text "This is an h2"
+    let h2Text = document.createTextNode("This is an h2");
+    div.appendChild(h2Text);
+    // append it to the same div the h1 one is
+    div.appendChild(h2);
+    // give a class name
+    // let h2ClassName = div.className = "h2";
+
+    /* Numbers 6 & 7 */
+    let h3 = document.createElement("h3");
+    let h3Text = document.createTextNode("This is an h3");
+    div.appendChild(h3Text);
+    div.appendChild(h3);
+
+    let h4 = document.createElement("h4");
+    let h4Text = document.createTextNode("This is an h4");
+    div.appendChild(h4Text);
+    div.appendChild(h4);
+
+    let h5 = document.createElement("h5");
+    let h5Text = document.createTextNode("This is an h5");
+    div.appendChild(h5Text);
+    div.appendChild(h5);
+
+    let h6 = document.createElement("h6");
+    let h6Text = document.createTextNode("This is an h6");
+    div.appendChild(h6Text);
+    div.appendChild(h6);
+    
+    
+
+    
+})
+
+
+    
+
+// /* Number 6 */
+// let h3 = document.createElement("h3");
+// let h3Text = document.createTextNode("This is an h3");
+// div.appendChild(h3);
+// let h3ClassName = div.className = "h3";
+
+// let h4 = document.createElement("h4");
+// let h4Text = document.createTextNode("This is an h4");
+// div.appendChild(h4);
+// let h4ClassName = div.className = "h4";
+
+// let h5 = document.createElement("h5");
+// let h5Text = document.createTextNode("This is an h5");
+// div.appendChild(h5);
+// let h5ClassName = div.className = "h5";
+
+// let h6 = document.createElement("h6");
+// let h6Text = document.createTextNode("This is an h6");
+// div.appendChild(h6);
+// let h6ClassName = div.className = "h6";
+
+// let eightColorsArray = ["#ff2d7c", "#335cb7", "#8651b0", "#018117", "#a5fcf1", "#2e8145", "#eaf0e2", "#2f5f4e"];
+
+// let button = document.createElement("button");
+// let buttonChangeColor = document.createElement("Click to change colors");
+
+
+// button.addEventListener("dblclick", function() {
+//     let createDiv = document.createElement("div");
+//     createDiv.style.color = eightColorsArray[2];
+//     createDiv.style.backgroundColor = eightColorsArray[0];
+// })
+
+
 
 
 
