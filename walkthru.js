efca04f8682d186855edca9b04b26a8a084280cd
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-03 10:04:17 -0600

    added class names to all heading tags and created loop for colorsArray

--- walkthru.js
+++ walkthru.js
@@ -1,6 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let container = document.createElement("div");
+    let colorsArray = ["#ff0018", "#ffa52c", "#ffff41", "#008018", "#0000f9", "#86007d", "#3c280d", "#cfb53b"];
+
     container.className = "header-container";
 
     let h1 = document.createElement("h1");
@@ -24,12 +26,24 @@ document.addEventListener("DOMContentLoaded", function() {
     h5.appendChild(h5Text);
     h6.appendChild(h6Text);
 
+    h1.className = "h1";
+    h2.className = "h2";
+    h3.className = "h3";
+    h4.className = "h4";
+    h5.className = "h5";
+    h6.className = "h6";
+
     container.appendChild(h1);
     container.appendChild(h2);
     container.appendChild(h3);
     container.appendChild(h4);
     container.appendChild(h5);
     container.appendChild(h6);
+
+    for (let i = 0; i < colorsArray.length; i++) {
+        console.log(colorsArray[i]);
+        // console.log("text " += colorsArray[i] + "<br>");
+    }
     
     document.body.appendChild(container);
 })
